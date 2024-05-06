50b53a03f6e608c39eb58d5a92e80b6098cc4c75
gamandeepsingh <gamandeepsingh4@gmail.com>
2024-05-06 14:00:22 +0530

    modal updated

--- saimlds/src/components/Domain.js
+++ saimlds/src/components/Domain.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Modal from 'react-modal';
+import { IoCloseCircle } from "react-icons/io5";
+
 const customStyles = {
   content: {
     top: '50%',
@@ -8,36 +10,41 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
-    backgroundColor:"black"
+    backgroundColor:"black",
+    borderRadius:"20px"
   },
 };
 
 const domain = [
   {
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_R2CaqMHVCpnln0Lt3KQPlYdU5kvFZLXJAmvMy_dGpw&s",
-    name: "AI/Ml Workshop",
+    name: "AI/Ml Workshop1",
     link: "https://www.google.com/",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting"
   },
   {
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_R2CaqMHVCpnln0Lt3KQPlYdU5kvFZLXJAmvMy_dGpw&s",
-    name: "AI/Ml Workshop",
+    name: "AI/Ml Workshop2",
     link: "https://www.google.com/",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting"
   },
   {
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_R2CaqMHVCpnln0Lt3KQPlYdU5kvFZLXJAmvMy_dGpw&s",
-    name: "AI/Ml Workshop",
+    name: "AI/Ml Workshop3",
     link: "https://www.google.com/",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting"
   },
   {
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_R2CaqMHVCpnln0Lt3KQPlYdU5kvFZLXJAmvMy_dGpw&s",
-    name: "AI/Ml Workshop",
+    name: "AI/Ml Workshop4",
     link: "https://www.google.com/",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting"
   },
 ];
 
 const Domain = () => {
   const [modalIsOpen, setIsOpen] = React.useState(false);
-  const [selectedDomain, setSelectedDomain] = React.useState(null);
+  const [selectedDomain, setSelectedDomain] = React.useState("");
 
   const openModal = (domain) => {
     setSelectedDomain(domain);
@@ -50,6 +57,7 @@ const Domain = () => {
 
   return (
     <>
+      <div >
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
@@ -57,17 +65,18 @@ const Domain = () => {
         contentLabel="Domain Modal"
       >
         {selectedDomain && (
-          <div style={{zIndex:"1000" }}>
+          <div style={{zIndex:"1000",position:"relative" }}>
             <h2 >
               {selectedDomain.name}
             </h2>
             <div style={{color:"white"}}>
               <p>Description: {selectedDomain.description}</p>
-              <button onClick={closeModal} style={{color:"white"}}>Close</button>
             </div>
+              <button onClick={closeModal} style={{color:"white",position:"absolute",top:0,right:0}}><IoCloseCircle size={30}/></button>
           </div>
         )}
       </Modal>
+      </div>
 
       <section className="skill" id="skills">
         <div className="container">
@@ -93,7 +102,7 @@ const Domain = () => {
                       <br />
                       {val.name}
                       <br />
-                      <button className="name" onClick={() => openModal(val)}>
+                      <button className="name" onClick={() =>{ openModal(val); setSelectedDomain(val)}}>
                         Check out more here!
                       </button>
                     </div>
